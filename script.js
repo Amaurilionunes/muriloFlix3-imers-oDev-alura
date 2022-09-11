70241dfe69977bac924cd4f7cf612cfd7332cfc9
Amaurilio Nunes de Azevedo filho <murilo84824187@gmail.com>
2022-09-11 16:00:10 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var listaFilmes = [
+  "https://upload.wikimedia.org/wikipedia/pt/1/1b/Schoolrockposter.jpg",
+  "https://http2.mlstatic.com/D_NQ_NP_2X_902444-MLB48346322776_112021-F.webp",
+  "https://http2.mlstatic.com/D_NQ_NP_2X_911647-MLB32407994257_102019-F.webp",
+  "https://http2.mlstatic.com/D_NQ_NP_2X_844170-MLB47807828246_102021-F.webp",
+  "https://th.bing.com/th/id/R.3121f041ac993c3ca70cd6d67cb0e00d?rik=lwKCSzFC8ryosw&riu=http%3a%2f%2f2.bp.blogspot.com%2f-OKdAYaQSGNo%2fUeHoTArXSDI%2fAAAAAAAANOw%2fpQc-iA_mpoE%2fs1600%2ffoto-artistica-russell-crowe-filme-gladiador.jpg&ehk=VPLypdZPX93%2bqFvo0yC5fggraEzqEKCP9RbEBpdeAvQ%3d&risl=&pid=ImgRaw&r=0"
+];
+for (i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
